04a99c7201eec129d1a486824dde136402c673d7
Ahmet Sayran <155922956+ahmetsayran@users.noreply.github.com>
2026-06-23 15:36:39 +0300

    Add JWT strategy and auth guard

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../../users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  async validate(payload: { sub: number }) {
+    try {
+      return await this.usersService.findById(payload.sub);
+    } catch {
+      throw new UnauthorizedException();
+    }
+  }
+}
