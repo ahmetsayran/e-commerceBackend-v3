65f6aaafbbbe6d8aa8d398b608a026275546e91f
Ahmet Sayran <155922956+ahmetsayran@users.noreply.github.com>
2026-06-24 14:36:23 +0300

    Implement ProductPhotosController endpoints

--- src/product-photos/product-photos.controller.ts
+++ src/product-photos/product-photos.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProductPhotosService } from './product-photos.service';
+import { CreateProductPhotoDto } from './dto/create-product-photo.dto';
+import { UpdateProductPhotoDto } from './dto/update-product-photo.dto';
 
 @Controller('product-photos')
-export class ProductPhotosController {}
+export class ProductPhotosController {
+  constructor(private readonly productPhotosService: ProductPhotosService) {}
+
+  @Post()
+  create(@Body() dto: CreateProductPhotoDto) {
+    return this.productPhotosService.create(dto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateProductPhotoDto,
+  ) {
+    return this.productPhotosService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productPhotosService.remove(id);
+  }
+}
