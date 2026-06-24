3f6daa360131f2923e36c17e06ab5783a95d59f3
Ahmet Sayran <155922956+ahmetsayran@users.noreply.github.com>
2026-06-24 05:11:38 +0300

    Implement ProductsService

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateProductDto) {
+    return this.prisma.product.create({ data: dto });
+  }
+
+  findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  async findById(id: number) {
+    const product = await this.prisma.product.findUnique({ where: { id } });
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    return product;
+  }
+
+  async update(id: number, dto: UpdateProductDto) {
+    await this.findById(id);
+    return this.prisma.product.update({ where: { id }, data: dto });
+  }
+
+  async remove(id: number) {
+    await this.findById(id);
+    await this.prisma.product.delete({ where: { id } });
+  }
+}
