a7d1ecbc27daaa4175de99aa0c12bdc2cac3ea4b
Ahmet Sayran <155922956+ahmetsayran@users.noreply.github.com>
2026-06-25 23:16:30 +0300

    Add PermissionsGuard

---
+++ src/common/guards/permissions.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { PERMISSION_KEY } from '../decorators/require-permission.decorator';
+
+@Injectable()
+export class PermissionsGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const required = this.reflector.getAllAndOverride<string>(PERMISSION_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!required) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+    const permissions: string[] = user?.permissions ?? [];
+
+    return permissions.includes(required);
+  }
+}
